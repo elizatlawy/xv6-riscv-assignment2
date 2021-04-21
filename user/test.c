ffceee1f218c3d4f3e1372b0e9412b2b52a0512b
eli <eli.zatlawy@gmail.com>
2021-04-21 15:11:59 +0300

    checking sigaction syscall

--- user/test.c
+++ user/test.c
@@ -8,20 +8,34 @@ void sigprocmastk_test(){
     uint newmask = (1 << 9);
     int oldmask = sigprocmask(newmask);
     printf("parent old mask %d\n", oldmask);
-//    int pid = fork();
-//    if(pid == 0){
-//        uint newmask = (1 << 17);
-//        uint oldmask = sigprocmask(newmask);
-//        printf("child old mask%d\n", oldmask);
-//    }
+    int pid = fork();
+    if(pid == 0){
+        uint newmask = (1 << 17);
+        uint oldmask = sigprocmask(newmask);
+        printf("child old mask %d\n", oldmask);
+    }
+}
+
+void handler(){
+    printf("Hey");
+}
+
+void sigaction_test(){
+    struct sigaction act;
+//    int sig_kill = 9;
+    act.sa_handler = &handler;
+    act.sigmask = (1 << 9);
+    struct sigaction old_act;
+    sigaction(15,&act,&old_act);
+    printf("old act sigmask %d", old_act.sigmask);
 
 }
 
 int main(int argc, char *argv[]) {
+    sigaction_test();
+//    sigprocmastk_test();
 
-    sigprocmastk_test();
-//    fprintf(1,"test\n");
 
-return 0;
+    exit(0);
 }
 
